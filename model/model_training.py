0ff7a166384ea7ebe1623bf655a957db9e120526
lokesh <lokeshwar-reddy.peram@epita.fr>
2025-10-05 20:22:52 +0200

    Initiated model branch Structure

---
+++ model/model_training.py
@@ -0,0 +1,12 @@
+# model_training.py
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from sklearn.ensemble import RandomForestClassifier
+import pickle
+ 
+# Placeholder code
+def train_model():
+    print("This is a placeholder for training the ML model.")
+ 
+if __name__ == "__main__":
+    train_model()
